9ba2f6a4ef935538de817197c097c902d8f984dc
Ederson Davi <silva.edersonqueiroz@gmail.com>
2020-05-19 19:06:20 -0300

    Create style css

---
+++ Assets/javascript/script.js
@@ -0,0 +1,15 @@
+const front = "card-front"
+const back = "card-back"
+
+let techs = [
+  "bootstrap",
+  "css",
+  "electron",
+  "firebase",
+  "html",
+  "javascript",
+  "jquery",
+  "mongo",
+  "node",
+  "react"
+]
